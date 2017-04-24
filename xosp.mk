39fe9c5b76c101857fd99d7882cae891769ff223
Alexis Viguié <alexis@hexagon.home>
2017-04-24 12:48:46 +0000

    XOSP bringup

---
+++ xosp.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/htc/m7/full_m7.mk)
+
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
+PRODUCT_NAME := xosp_m7
+
+# Override build props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys" \
+    BUILD_ID=LMY47O.H18 \
+    PRIVATE_BUILD_DESC="6.04.1700.18 CL536258 release-keys"
